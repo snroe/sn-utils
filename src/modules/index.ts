cba60b12c91c0a3802fb3ae4cb98a213bdf0bab5
efahnjoe <efahnjoe@gmail.com>
2025-07-06 15:40:11 +0800

    refactor(modules): 将 freeze.ts 重命名为 deepFreeze.ts
    - 更新了模块索引文件中的引用，以匹配新的文件名

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,5 +1,5 @@
 export * from './hash/index.js';
 export * from './uuid/index.js';
 export * from './delay.js';
-export * from './freeze.js';
+export * from './deepFreeze.js';
 export * from './randArray.js';
